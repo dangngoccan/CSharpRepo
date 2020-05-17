2e1c6b9fc229aae44f78a9ac9bb0471142433454
candang <dangngoccan@outlook.com>
2020-05-17 21:15:18 +0700

    First console project

---
+++ PrismStart/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace PrismStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            while(true)
+            {
+                var input = Console.ReadLine();
+
+                if (commands.Contains(input))
+                {
+                    int index = Array.IndexOf(commands, input);
+
+                    int X = int.Parse(Console.ReadLine());
+                    int Y = int.Parse(Console.ReadLine());
+
+                    int result = funcs[index](X, Y);
+
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Mistake!");
+                }
+            }
+        }
+
+        static int Add(int x, int y)
+        {
+            return (x + y);
+        }
+
+        static int Sub(int x, int y)
+        {
+            return (x - y);
+        }
+
+        static int Mul(int x, int y)
+        {
+            return (x * y);
+        }
+
+        static int Div(int x, int y)
+        {
+            return (x / y);
+        }
+
+        static string[] commands = { "Add", "Sub", "Mul", "Div" };
+        static Func<int, int, int>[] funcs = { Add, Sub, Mul, Div };
+
+    }
+}
